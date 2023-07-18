be85499e29fbd25bd10eae8a51cfc313b42633f6
Anatoly <azs.zavyalov@gmail.com>
2023-07-18 13:45:44 -0400

    Add chmod in build

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 # to run the build with tests, add the "-t" option. For instance, instead of running "build.sh", run "build.sh -t".
 
+SCRIPTNAME="walnut.sh"
+
 if [[ $1 = "-t" ]]; then
 	# run with tests
 	echo "Building Walnut and running tests."
@@ -11,5 +13,7 @@ else
 	echo "Building Walnut. To run tests, add the -t flag to the command."
 	./gradlew clean customFatJar
 fi	
+
+chmod +x $SCRIPTNAME
 
 read -p "Press enter to continue"
